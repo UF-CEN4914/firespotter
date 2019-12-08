4bbc0e8335c76806caa6d3087433364d46839143
Luke Pfeiffer <lukerpfeiffer@gmail.com>
2019-12-07 20:57:59 -0500

    Bootstrapify

--- firespotter/settings.py
+++ firespotter/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'firespotter.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        "DIRS": ["firespotter/templates/"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
