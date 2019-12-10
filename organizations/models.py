d4f26bbe971be38d4ab39fc5439641822938207a
Luke Pfeiffer <lukerpfeiffer@gmail.com>
2019-12-09 20:16:13 -0500

    Organization model design

--- organizations/models.py
+++ organizations/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Organization(models.Model):
+    name = models.CharField(max_length=200)
+    country = models.CharField(max_length=30)
+    region = models.CharField(max_length=30)
+    timezone = models.CharField(max_length=100)
+    email = models.CharField(max_length=300)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
