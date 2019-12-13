123a44dd9541742d351ea018f6f57ec80333652b
Luke Pfeiffer <lukerpfeiffer@gmail.com>
2019-12-13 13:32:56 -0500

    Update organizations to remove timezone and add phone number

--- organizations/models.py
+++ organizations/models.py
@@ -5,7 +5,7 @@ class Organization(models.Model):
     name = models.CharField(max_length=200)
     country = models.CharField(max_length=30)
     region = models.CharField(max_length=30)
-    timezone = models.CharField(max_length=100)
     email = models.CharField(max_length=300)
+    phone_num = models.CharField(max_length=20)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
