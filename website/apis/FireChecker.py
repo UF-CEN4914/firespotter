a698e0b63753307b8350e3ffc4ba1631a191cee1
John Hodson <hodsonus@gmail.com>
2020-02-09 23:55:16 -0500

    changes made to website, needs testing but website is broken on local

--- website/apis/FireChecker.py
+++ website/apis/FireChecker.py
@@ -1,6 +1,28 @@
-import random
+import torch
+import cv2
+import numpy as np
+
+IMG_SIZE = 100
 
 class FireChecker():
+
+    wildfire_model = torch.load('wfm.pt')
+
     @staticmethod
     def IsWildFire(img_path):
-        return random.choice([True, False])
+        #  read the image and resize it 
+        img = cv2.imread(img_path)
+        img = cv2.resize(img, (IMG_SZ, IMG_SZ))
+
+        # convert the image to a tensor and view it in the form that the model expects (3 channels, IMG_SZxIMG_SZ)
+        img = (torch.tensor(img)/255.0).view(-1,3,IMG_SZ,IMG_SZ)
+
+        #  run the image through the model and grab the 0th result (returns a list bc of batching)
+        output = wildfire_model(img)[0]
+
+        # output is a probability, 0 is no fire and 1 is fire
+        predicted_class = np.round(output.detach())
+
+        # cast the predicted class to a boolean
+        if predicted_class == 1: return True
+        else return False
